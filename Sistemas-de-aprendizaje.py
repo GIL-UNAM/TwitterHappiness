06b258b16c6c83060f4b495235fa005b28dc4f83
KarlaMendoza75 <72536241+KarlaMendoza75@users.noreply.github.com>
2020-10-14 20:00:44 -0500

    Update Sistemas-de-aprendizaje.py

--- Sistemas-de-aprendizaje.py
+++ Sistemas-de-aprendizaje.py
@@ -49,72 +49,10 @@ def clean_text(raw_text):
 
 if __name__ == "__main__":
 
-	text1 = pd.read_csv('alegria_claudia_terminado.csv', names = ['Etiqueta1', 'Tweet1'])
-	text1['Tweet1'] = text1['Tweet1'].replace('http\S+', '', regex=True).replace(r'www\S+', '', regex=True)
+	dataset = pd.read_csv('Dataset.csv')
 
-	text2 = pd.read_csv('alegria_tona_terminado.csv', names = ['Etiqueta2', 'Tweet2'])
-	text2['Tweet2'] = text2['Tweet2'].replace('http\S+', '', regex=True).replace(r'www\S+', '', regex=True)
-
-	text3 = pd.read_csv('alegria_iris.csv', names = ['Etiqueta3', 'Tweet3'])
-	text3['Tweet3'] = text3['Tweet3'].replace('http\S+', '', regex=True).replace(r'www\S+', '', regex=True)
-
-	tweets = pd.concat([rowst(text1), rowst(text2), rowst(text3)])
-
-	etiquetas = pd.concat([rowse(text1), rowse(text2), rowse(text3)])
-	etiquetas = etiquetas.replace(' N', 'N')
-	etiquetas = etiquetas.replace('N ', 'N')
-	etiquetas = etiquetas.replace('X', 'C')
-	etiquetas = etiquetas.replace('V', 'C')
-	etiquetas = etiquetas.replace('S', 'A')
-	etiquetas = etiquetas.replace('D', 'A')
-	etiquetas = etiquetas.replace('O', 'P')
-	etiquetas = etiquetas.replace(' F', 'F')
-	etiquetas = etiquetas.replace('C ', 'C')
-	etiquetas = etiquetas.replace('Á', 'A')
-
-	data = pd.DataFrame({'et1': rowse(text1), 'et2': rowse(text2),
-		'et3': rowse(text3) , 'twits': rowst(text1)})
-	data['et1'] = np.where(data['et1'] == 'Á', 'A', data['et1'])
-	data['et2'] = np.where(data['et2'] == 'X', 'C', data['et2'])
-	data['et2'] = np.where(data['et2'] == 'V', 'C', data['et2'])
-	data['et2'] = np.where(data['et2'] == 'S', 'A', data['et2'])
-	data['et2'] = np.where(data['et2'] == 'D', 'A', data['et2'])
-	data['et2'] = np.where(data['et2'] == ' N', 'N', data['et2'])
-	data['et3'] = np.where(data['et3'] == 'N ', 'N', data['et3'])
-	data['et3'] = np.where(data['et3'] == 'O', 'P', data['et3'])
-	data['et3'] = np.where(data['et3'] == ' F', 'F', data['et3'])
-	data['et3'] = np.where(data['et3'] == 'C ', 'C', data['et3'])
-	data['et3'] = np.where(data['et3'] == 'V', 'C', data['et3'])
-
-	et1 = list(enumerate(data['et1']))
-	et2 = list(enumerate(data['et2']))
-	et3 = list(enumerate(data['et3']))
-
-	atl2 = set(et1).intersection(et2).union(set(et1).intersection(et3)).union(set(et2).intersection(et3))
-	ex3 = set(et1).intersection(et2).intersection(set(et1).intersection(et3))
-
-	union = list(sorted(atl2.union(ex3)))
-
-	index = [x for x,y in et1]
-	iunion = [x for x,y in union]
-	eunion = [y for x,y in union]
-
-	etiq = pd.Series(eunion, index=iunion)
-	data['match'] = etiq.reindex()
-	data = data.fillna('NA')
-
-
-		
-	text_to_clean = list(data['twits'])
-	cleaned_text = apply_cleaning_function_to_list(text_to_clean)
-	data['cleaned'] = cleaned_text
-	new_data = pd.DataFrame({'text': data.cleaned, 'target': data.match})
-	new_data = new_data[new_data.target != 'NA'] 	#Se eliminan los que quedaron sin categoría
-	#print(new_data)
-
-
-	X = new_data['text']
-	y = new_data['target']
+	X = dataset['text']
+	y = dataset['target']
 
 	skf = StratifiedKFold(n_splits=3)
 	for train_index,test_index in skf.split(X,y):
